a3e70416a6b11cec026c5d0f1070d7759fbe44b0
hyunhyesoo <sue990831@gmail.com>
2026-06-02 09:49:52 +0900

    9장 중복되는 도서번호(bookId) 유효성 검사 사용자 정의 @을 생성해서 constrainValidator로 구현

---
+++ src/main/java/kr/ac/kopo/hhs/bookmarket/validator/BookIdValidator.java
@@ -0,0 +1,34 @@
+package kr.ac.kopo.hhs.bookmarket.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import kr.ac.kopo.hhs.bookmarket.domain.Book;
+import kr.ac.kopo.hhs.bookmarket.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BookIdValidator implements ConstraintValidator<BookId, String> {
+    @Autowired
+    private BookService bookService;
+
+    @Override
+    public void initialize(BookId constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String bookId, ConstraintValidatorContext constraintValidatorContext) {
+        //도서아이디 중복이면 false
+        //book 참조변수: bookID가 중복되면 Book 객체 참조값 반환, 존재하지 않으면 null 반환
+        Book book;
+        try {
+            book = bookService.getBookById(bookId);
+        } catch (RuntimeException e) {
+            return true;
+        }
+        //중복되는 경우
+        if (book != null)
+            return false;
+
+        return true;
+    }
+}
